f050d758f083973a792ae5d7549ef8443a569873
Janie <janieqing2024@gmail.com>
2023-12-05 10:15:51 -0500

    easy and medium activity sample code

---
+++ activity1-sample-answers/activity1-easy.py
@@ -0,0 +1,7 @@
+
+print("User_movie")
+movie_ticket = 10
+popcorn = 5
+money = 2
+
+print(movie_ticket + popcorn + money)
---
+++ activity1-sample-answers/activity1-medium.py
@@ -0,0 +1,12 @@
+
+print("User_movie")
+
+ticket = 10
+popcorn = 5
+candy = 3
+
+total_cost = (1 * ticket) + (1 * popcorn) + (1 * candy)
+
+final_amount = total_cost - 2
+
+print(final_amount)
