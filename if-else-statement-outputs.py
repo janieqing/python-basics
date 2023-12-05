dbc568467aa754efa7d89e4ba54d3973c91425c0
Janie <janieqing2024@gmail.com>
2023-12-04 19:05:51 -0500

    added if-else-statment examples

---
+++ if-else-statement-outputs.py
@@ -0,0 +1,14 @@
+# Python If-Else Statements
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is less than a")
+
+name = "Octo"
+if name == "Tom":
+   print("name is Tom, incorrect.")
+else:
+   print("name is not Tom.")
