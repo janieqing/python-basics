067871e6142a1635622afbd8524d4ac2aa5e1ffc
Janie <janieqing2024@gmail.com>
2023-12-04 19:09:52 -0500

    updated input examples into git

---
+++ input-outputs.py
@@ -0,0 +1,13 @@
+# Python Input 
+
+print("Examples of String Input:")
+x = input("Enter your name: ")
+print(x)
+
+
+password = input("What is the password? ")
+print(password)
+
+print("Examples of Integer Input:")
+number = int(input("Enter a number: "))
+print(number)
