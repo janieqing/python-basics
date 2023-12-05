fa7140787cea6910d0147a5557bb2c39c87d58eb
Janie <janieqing2024@gmail.com>
2023-12-04 19:03:57 -0500

    added basic math examples

---
+++ basic-math-outputs.py
@@ -0,0 +1,23 @@
+# Python Basic Math
+
+print("Examples of Basic Math in Python")
+math1 = 4 + 15
+print(math1)
+math2 = 24 - 16
+print(math2)
+math3 = 5 * 3
+print(math3)
+math4 = 16/4
+print(math4)
+
+print("Examples of Basic Math in Python with Variables")
+a = 4
+b = 2
+add = a + b
+sub = a - b
+mult = a * b
+divide = a / b
+print(add)
+print(sub)
+print(mult)
+print(divide)
